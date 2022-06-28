358ab81791a55db81e4cd181b0fbb685a73e321a
Abdullahsaeed24 <94451632+Abdullahsaeed24@users.noreply.github.com>
2022-06-28 13:19:48 +0200

    build private methods

--- CCR/CCR.py
+++ CCR/CCR.py
@@ -10,18 +10,20 @@ import random
 
 class VUT_FileAnalysis:
 
-    def __init__(self,FileleName:"str",TestData:list) -> None:
+    def __init__(self,FileleName:"str") -> None:
 
         ''' class instance Constractor
         INPUT: FileleName --> the file name
         OUTPUT: NO return
         '''
         self.FileleName = FileleName
-        self.TestData = TestData
-        self.NewFileName ="FileAnalysis"
+        self.TestData = []
+        self.DataFrame = None
 
 
-    def FileData_Handling(self)->str:
+    # privite method
+    def __FileData_Handling(self)->str:
+
         ''' handling and adjusting the file -> isolate the Not Flat file data and handling the semicolon seperated data
         INPUT: No INPUT
         OUTPUT: NO return
@@ -38,7 +40,30 @@ class VUT_FileAnalysis:
                     self.TestData.append(l)
 
 
+    # privite method
+    def __Get_Rows_Length(self) -> None:
 
+        ''' handling and adjusting the file -> isolate the Not Flat file data and handling the semicolon seperated data
+        INPUT: No INPUT
+        OUTPUT: NO return
+        '''
+        self.__FileData_Handling()
+        return len(self.TestData)+1
+
+
+
+    def Get_Test_Headers(self) -> list:
+        self.__FileData_Handling()
+        return self.TestData
+
+
+    def Read_CSV_File (self) -> None:
+        self.__FileData_Handling()
+        self.DataFrame = pd.read_csv(self.FileleName , sep = ";" , skiprows = self.__Get_Rows_Length())
+
+
+
+'''
         self.NewFileName ="FileAnalysis"+self.FileleName
         file = open(self.NewFileName,"w")
 
@@ -52,45 +77,17 @@ class VUT_FileAnalysis:
                 else:
                     if l != "END_OF_HEADER":
                         file.write(line)
+
         file.close()
         return self.NewFileName
-
-
-    def Get_TestData(self) -> list:
-        return self.TestData
-
-
-'''
-TestData = []
-
-with open("TRIAL_220616_110008_00401_FBl_26_AUTOSAVE.txt") as f:
-    for line in f:
-        l = line.strip()
-        if l == "END_OF_HEADER":
-            break
-        else:
-            TestData.append(l)
-
-
-file = open("trail_220616_110008_00401_FBl_26_AUTOSAVE.txt","w")
-
-with open("TRIAL_220616_110008_00401_FBl_26_AUTOSAVE.txt") as f:
-    for line in f:
-        l = line.strip()
-        if (l in TestData):
-            continue
-        else:
-            if l != "END_OF_HEADER":
-                file.write(line)
-file.close()
-
-
-df = pd.read_csv("trail_220616_110008_00401_FBl_26_AUTOSAVE.txt",sep=";")
-
-print(df.head())
 '''
 
-vut = VUT_FileAnalysis("TRIAL_220616_110008_00401_FBl_26_AUTOSAVE.txt",[])
-file = vut.FileData_Handling()
-df = pd.read_csv(file,sep=";", skiprows=)
-print(df.head())
+
+
+
+
+
+df = VUT_FileAnalysis("TRIAL_220616_110008_00401_FBl_26_AUTOSAVE.txt")
+df.Read_CSV_File()
+print(df.DataFrame.head())
+print(df.DataFrame.describe())
